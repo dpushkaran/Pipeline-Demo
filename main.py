bf52130b2bcb9288a45802060e116397acf55c5c
Devadarshan Pushkaran <91815543+dpushkaran@users.noreply.github.com>
2026-02-04 22:30:28 -0600

    updated to be PDF -> Text

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-
 load_dotenv()
-
 client = OpenAI()
 
-def generate_response(user_prompt):
-
+def generate_response(pdf_path):
+    file = client.files.create(
+        file=open(pdf_path, "rb"),
+        purpose="user_data"
+    )
     messages = [
-        {"role": "system", "content": "You are an professor with years of experience in teaching and research about computer scince."},
-        {"role": "user", "content": user_prompt}
+        {"role": "system", "content": "You are a professor with years of experience in teaching and research about computer science."},
+        {
+            "role": "user",
+            "content": [
+                {"type": "file", "file": {"file_id": file.id}},
+                {"type": "text", "text": "Summarize this PDF with key points and conclusions."}
+            ]
+        }
     ]
-
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=messages,
         temperature=.5,
-        max_tokens=100,
+        max_tokens=2000,
     )
-
     result = response.choices[0].message.content
-
+    client.files.delete(file.id)
     return result
 
 if __name__ == "__main__":
-    prompt = "Explain what a variable is in python in a short and simple way"
-    answer = generate_response(prompt)
+    pdf_path = "./04 Intro to Probability.pdf"
+    answer = generate_response(pdf_path)
     
     with open("output.txt", "w") as file:
         file.write(answer)
